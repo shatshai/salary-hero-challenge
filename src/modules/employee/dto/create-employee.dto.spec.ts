c77bbf271715d34f303af6fc9d3d0071eae7ac2a
Shatshai Saeaio <shatshai@gmail.com>
2024-05-03 22:27:40 +0700

    Add CreateEmployeeDto

---
+++ src/modules/employee/dto/create-employee.dto.spec.ts
@@ -0,0 +1,49 @@
+import { plainToInstance } from 'class-transformer'
+import { validate } from 'class-validator'
+import { CreateEmployeeDto } from './create-employee.dto'
+import { SalaryTypes } from '@app/config/constants'
+
+describe('CreateEmployeeDto', () => {
+  // Reset mocks and timers after each test
+  afterEach(() => {
+    jest.useRealTimers()
+    jest.clearAllMocks()
+  })
+
+  it('Given correct data - should validate without error', async () => {
+    const employee = {
+      username: 'some-username',
+      email: 'user@example.com',
+      companyId: 2,
+      salaryTypeId: SalaryTypes.MONTHLY,
+      salary: 3000,
+      payDate: 25,
+    }
+    const dto = plainToInstance(CreateEmployeeDto, employee)
+    const errors = await validate(dto)
+    expect(errors.length).toBe(0)
+  })
+
+  it('Given pay date as null - should validate error', async () => {
+    const employee = {
+      username: 'some-username',
+      email: 'user@example.com',
+      companyId: 2,
+      salaryTypeId: SalaryTypes.MONTHLY,
+      salary: 3000,
+      payDate: null,
+    }
+    const dto = plainToInstance(CreateEmployeeDto, employee)
+    const errors = await validate(dto)
+    expect(errors.length).toBe(1)
+    expect(errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          constraints: expect.objectContaining({
+            isInt: 'payDate must be an integer number',
+          }),
+        }),
+      ]),
+    )
+  })
+})
