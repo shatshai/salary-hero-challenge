2fda611614f754bbd44dadc2488151031fe1d047
Shatshai Saeaio <shatshai@gmail.com>
2024-05-03 19:45:13 +0700

    Fixing default payment date to 1st date of month

--- docker/mysql-init.sql
+++ docker/mysql-init.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS employee (
   company_id INT NOT NULL,
   salary DOUBLE(10, 2) UNSIGNED NOT NULL,
   salary_type_id TINYINT UNSIGNED NOT NULL,
-  pay_date TINYINT UNSIGNED NULL,
+  pay_date TINYINT UNSIGNED NOT NULL DEFAULT 1,
   KEY idx_username (username),
   KEY idx_email (email),
   KEY idx_company (company_id)
@@ -47,6 +47,6 @@ SET @salary_type3 := (SELECT id FROM salary_type WHERE type = 'monthtodate');
 
 SET @company1 := (SELECT id FROM company WHERE name = 'company name1');
 SET @company2 := (SELECT id FROM company WHERE name = 'company name2');
-INSERT INTO employee (username, email, company_id, salary, salary_type_id, pay_date) VALUES ('user1', 'user1@example.com', @company1, 700, @salary_type1, null);
-INSERT INTO employee (username, email, company_id, salary, salary_type_id, pay_date) VALUES ('user2', 'user2@example.com', @company2, 30000, @salary_type2, null);
+INSERT INTO employee (username, email, company_id, salary, salary_type_id, pay_date) VALUES ('user1', 'user1@example.com', @company1, 700, @salary_type1, DEFAULT);
+INSERT INTO employee (username, email, company_id, salary, salary_type_id, pay_date) VALUES ('user2', 'user2@example.com', @company2, 30000, @salary_type2, DEFAULT);
 INSERT INTO employee (username, email, company_id, salary, salary_type_id, pay_date) VALUES ('user3', 'user3@example.com', @company2, 30000, @salary_type3, 25);
