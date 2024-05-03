4905063cfb31d83ef93932d4df5528cf1f32eb4f
Shatshai Saeaio <shatshai@gmail.com>
2024-05-03 13:22:55 +0700

    Binding Swagger APIs documents

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // Binding Swagger APIs documents
+  const config = new DocumentBuilder()
+    .setTitle('Salary Hero challenge')
+    .setDescription('The employee salary API description')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
