687cf505a8facfe811155a71d4a200cf6347f6dc
Shatshai Saeaio <shatshai@gmail.com>
2024-05-05 19:54:50 +0700

    Add constants comment

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,9 @@
-export const INCLUDE_PAYMENT_DATE = true
+// Indicates whether payment date should be included in salary calculations
+export const INCLUDE_PAYMENT_DATE = true;
 
+// Enum defining different salary types - Map from Salary Type data table.
 export enum SalaryTypes {
-  DAILY = 1,
-  MONTHLY = 2,
-  MONTH_TO_DATE = 3,
+  DAILY = 1,            // Salary calculated on a daily basis
+  MONTHLY = 2,          // Salary calculated on a monthly basis
+  MONTH_TO_DATE = 3,    // Salary calculated from the start of the month to the current date
 }
