5e6b095fd532a0eb3e3a851f92d36e2af942032b
Shatshai Saeaio <shatshai@gmail.com>
2024-05-05 11:10:34 +0700

    Remove app contaoller and service that not using

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaModule } from '@app/infrastructure/prisma/prisma.module'
 import { EmployeeModule } from '@app/modules/employee/employee.module'
 import { LoggerMiddleware } from '@app/common/middleware/logger.middleware'
@@ -8,8 +6,6 @@ import { LoggerMiddleware } from '@app/common/middleware/logger.middleware'
 // Define the main application module
 @Module({
   imports: [PrismaModule, EmployeeModule], // Import the PrismaModule for database access and EmployeeModule
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
